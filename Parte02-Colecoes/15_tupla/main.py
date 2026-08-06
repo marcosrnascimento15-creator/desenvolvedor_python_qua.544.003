526b16e8a1b4d31353ca68b645cc3752721fceeb
marcosrnascimento15-creator <marcos.r.nascimento15@aluno.senai.br>
2026-08-05 21:34:26 -0300

    aula 05/08/2026

---
+++ Parte02-Colecoes/15_tupla/main.py
@@ -0,0 +1,11 @@
+meses = (
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho"
+)
+
+for mes in meses:
+    print(mes)
---
+++ Parte02-Colecoes/17_inserir_chave/main.py
@@ -0,0 +1,11 @@
+usuario = {
+    'nome': "Fulano de Tal",
+    'idade': 35,
+    'email': "fulano@gmail.com",
+    'cpf': "123.456.789-12"
+}
+
+# usuairo informa a vhave que deseja alterar
+chve = input("Informe o nome da chave: ").strip().lower()
+
+# TODO: verifica se a chave existe
