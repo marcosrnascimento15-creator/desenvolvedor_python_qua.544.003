6e815209fc0e641434cc5fdddd07de0353409ee4
marcosrnascimento15-creator <marcos.r.nascimento15@aluno.senai.br>
2026-08-13 21:26:49 -0300

    aula 13/08/26 def

---
+++ Parte03-Funcoes/02_parametro/main.py
@@ -0,0 +1,8 @@
+# Função com parâmetro
+def boas_vindas(nome):
+    print(f"Seja bem vindo, {nome}!")
+
+# Algoritimo Principal
+nome = input("Informe seu nome: ").strip().title()
+boas_vindas(nome)
+    
