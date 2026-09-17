6bea29a09972a1ab45f335b24625561b411e92cb
Gustavo <gusta.aadc@gmail.com>
2026-09-16 21:08:16 -0300

    Comit do dia 16/09/26

--- Parte07-Flask/04_poltergit/app.py
+++ Parte07-Flask/04_poltergit/app.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template, request
+import pyautogui as auto
 
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/comitar", methods = ['POST'])
+def comitar():
+    hoje = date.today().strftime("%d/%m/%y")
+    msg = None
+    repositorio = None
+    if request.method == "POST":
+        repositorio = request.form.get("repositorio","")
+    if repositorio:
+        auto.PAUSE = 1
+        auto.hotkey("win","r")
+        auto.write("cmd")
+        auto.press("enter")
+        auto.write(f"cd {repositorio}")
+        auto.press("enter")
+        auto.write("git add .")
+        auto.press("enter")
+        auto.write(f'git commit -m "Comit do dia {hoje}"')
+        auto.press("enter")
+        auto.write("git push")
+        auto.press("enter")
+        auto.sleep(3)
+        auto.write("exit")
+        auto.press("enter")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
