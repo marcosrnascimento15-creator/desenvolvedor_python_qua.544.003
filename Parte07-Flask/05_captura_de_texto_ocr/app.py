7e4674cb630d5352437c743f2b11ef745d4dc5fb
Gustavo <gusta.aadc@gmail.com>
2026-09-18 21:35:26 -0300

    Commit do dia 18/09/2026

--- Parte07-Flask/05_captura_de_texto_ocr/app.py
+++ Parte07-Flask/05_captura_de_texto_ocr/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/textoExtraido')
+def extrair_texto():
+    return render_template('extracao.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
